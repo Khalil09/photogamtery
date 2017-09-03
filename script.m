ac5bc327ea6fefda37a0ef7f7eadc6b46ef468df
khalil09 <khalilcarsten@gmail.com>
2017-09-03 18:01:11 -0300

    scond commit

--- script.m
+++ script.m
@@ -1,16 +1,25 @@
-im = imread('imagem1_linhas.jpg');
+im = imread('Imagem5_linhas.jpg');
 imshow(im);
-pt1 = ginput(1);
-pt2 = ginput(1);
-pt3 = ginput(1);
-pt4 = ginput(1);
-v = ginput(1);
+footP1 = ginput(1);
+headP1 = ginput(1);
+pointP1 = ginput(1);
+footP2 = ginput(1);
+headP2 = ginput(1);
+pointP2 = ginput(1);
+vanish_p = ginput(1);
 
-hr = sqrt(((pt1(1,1) - pt2(1,1)).^2) + ((pt1(1,2) - pt2(1,2)).^2));
-hinf = sqrt(((v(1,1) - pt2(1,1)).^2) + ((v(1,2) - pt2(1,2)).^2));
-hl = sqrt(((pt3(1,1) - pt4(1,1)).^2) + ((pt3(1,2) - pt4(1,2)).^2));
-hlinf = sqrt(((v(1,1) - pt3(1,1)).^2) + ((v(1,2) - pt3(1,2)).^2));
+hr = sqrt(((footP1(1,1) - pointP1(1,1)).^2) + ((footP1(1,2) - pointP1(1,2)).^2));
+hinf = sqrt(((vanish_p(1,1) - pointP1(1,1)).^2) + ((vanish_p(1,2) - pointP1(1,2)).^2));
+hl = sqrt(((pointP2(1,1) - footP2(1,1)).^2) + ((pointP2(1,2) - footP2(1,2)).^2));
+hlinf = sqrt(((vanish_p(1,1) - pointP2(1,1)).^2) + ((vanish_p(1,2) - pointP2(1,2)).^2));
 
-HR = 1.75;
+htrue = sqrt(((footP1(1,1) - headP1(1,1)).^2) + ((footP1(1,2) - headP1(1,2)).^2));
+htrue2 = sqrt(((footP2(1,1) - headP2(1,1)).^2) + ((footP2(1,2) - headP2(1,2)).^2));
+
+altura = 1.86;
+
+HR = (altura*hr)/htrue;
 
 H = HR*(hl*hinf)/(hr*hlinf);
+
+altura2 = H*htrue2/(hl);
