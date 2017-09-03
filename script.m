c65f827d48b86b50fba0cae21d95edeb5bcbfd00
khalil09 <khalilcarsten@gmail.com>
2017-09-03 16:32:01 -0300

    scond commit

--- script.m
+++ script.m
@@ -1,6 +1,16 @@
-im = imread('image');
+im = imread('imagem1_linhas.jpg');
 imshow(im);
-ln1 = int16(getline);
-line([ln1(1,1) ln1(2,1)], [ln1(1,2) ln1(2,2)]);
-ln2 = int16(getline);
-line([ln2(1,1) ln2(2,1)], [ln2(1,2) ln2(2,2)]);
+pt1 = ginput(1);
+pt2 = ginput(1);
+pt3 = ginput(1);
+pt4 = ginput(1);
+v = ginput(1);
+
+hr = sqrt(((pt1(1,1) - pt2(1,1)).^2) + ((pt1(1,2) - pt2(1,2)).^2));
+hinf = sqrt(((v(1,1) - pt2(1,1)).^2) + ((v(1,2) - pt2(1,2)).^2));
+hl = sqrt(((pt3(1,1) - pt4(1,1)).^2) + ((pt3(1,2) - pt4(1,2)).^2));
+hlinf = sqrt(((v(1,1) - pt3(1,1)).^2) + ((v(1,2) - pt3(1,2)).^2));
+
+HR = 1.75;
+
+H = HR*(hl*hinf)/(hr*hlinf);
